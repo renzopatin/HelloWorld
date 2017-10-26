e6a81cdcd9707a6a47f4a00ce8f3235aeca2a801
Renzo Malla <renzomalla97@gmail.com>
2017-10-26 14:04:53 -0500

    V 2.0 Three chickens more

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,7 +9,23 @@ public class HelloWorld{
 		chicken.initializeData("Lucy","coffee",2);
 		chicken.print();
 		
-	}
-	
+		Chicken chicken01;
+		chicken01 = new Chicken();
+		chicken.initializeData("Pepa","Red",3);
+		chicken.print();
+		chicken.layAnEgg();
+		
+		Chicken chicken02;
+		chicken02 = new Chicken();
+		chicken.initializeData("Jani","Yellow",2);
+		chicken.print();
+		chicken.poop();
+		
+		Chicken chicken03;
+		chicken03 = new Chicken();
+		chicken.initializeData("Pris","Black and White",1);
+		chicken.print();
+		chicken.drink();
+		
+	}	
 }
-
